0298910971549284c86b2a0be80f623cb77913b3
Daniel Jilg <daniel@telemetrydeck.com>
2022-06-06 13:55:34 +0200

    Allow starting a new Session from Home Screen

--- DubDubTrivia/DubDubTrivia/Views/ContentView.swift
+++ DubDubTrivia/DubDubTrivia/Views/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var triviaSessionIsActive: Bool = false
+    @State var isLoading: Bool = false
+    @State var triviaSessionManager: TriviaSessionManager?
+
     var body: some View {
-        Text("Hello, world!")
+        if isLoading {
+            ProgressView()
+        } else {
+            Button("Start a new Trivia Session") {
+                isLoading = true
+                Task {
+                    await initializeTriviaSessionManager()
+                }
+                triviaSessionIsActive = true
+                isLoading = false
+            }
             .padding()
+            .sheet(isPresented: $triviaSessionIsActive) {
+                if let triviaSessionManager = triviaSessionManager {
+                    TriviaSessionView(sessionManager: triviaSessionManager)
+                } else {
+                    Text("No Trivia Session Manager")
+                }
+            }
+        }
+    }
+
+    private func initializeTriviaSessionManager() async {
+        let url = Bundle.main.url(forResource: "opentdb_example", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        let apiResponse = try! decoder.decode(APIResponse.self, from: data)
+
+        let sessionManager = TriviaSessionManager()
+        sessionManager.questionQueue = apiResponse.results.shuffled()
+
+        self.triviaSessionManager = sessionManager
     }
 }
 
