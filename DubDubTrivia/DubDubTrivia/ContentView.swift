99600b89c56a12c93c63a9fa42c52bb68b513f60
Daniel Jilg <daniel@telemetrydeck.com>
2022-06-06 12:10:28 +0200

    Inital Commit

---
+++ DubDubTrivia/DubDubTrivia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DubDubTrivia
+//
+//  Created by Daniel Jilg on 06.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubDubTrivia/DubDubTrivia/DubDubTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubTriviaApp.swift
+//  DubDubTrivia
+//
+//  Created by Daniel Jilg on 06.06.22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
